fbcd7ad13fcc55e83a7271237bd10c4e004d6c31
kubertX <ikubert@163.com>
2023-09-21 00:07:57 +0800

    feat(数据结构和算法): HOT100

---
+++ src/main/java/leetcode/editor/cn/Lc_1_TwoSum.java
@@ -0,0 +1,21 @@
+package leetcode.editor.cn;
+
+
+public class Lc_1_TwoSum{
+    public static void main(String[] args) {
+    
+        Solution solution = new Lc_1_TwoSum()
+                            .new Solution();
+    }
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        return nums;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
