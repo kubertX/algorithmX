aa8e81bb2263728ff8b16f46d51996f7a2be3c27
kubertX <ikubert@163.com>
2023-11-15 01:17:02 +0800

    feat(国际电商):codetop

---
+++ src/main/java/leetcode/editor/cn/Lc_967_NumbersWithSameConsecutiveDifferences.java
@@ -0,0 +1,22 @@
+package leetcode.editor.cn;
+
+
+public class Lc_967_NumbersWithSameConsecutiveDifferences{
+    
+    public static void main(String[] args) {
+    
+        Solution solution = new Lc_967_NumbersWithSameConsecutiveDifferences()
+        .new Solution();
+    }
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] numsSameConsecDiff(int n, int k) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
